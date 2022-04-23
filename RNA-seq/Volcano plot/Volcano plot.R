362bc42838cf339413525efd967614658bdb4c21
Yue Li <96683864+YueLi9104@users.noreply.github.com>
2022-04-23 16:38:54 +0800

    update Volcano plot with larger size

--- RNA-seq/Volcano plot/Volcano plot.R
+++ RNA-seq/Volcano plot/Volcano plot.R
@@ -9,9 +9,11 @@ install.packages("IDPmisc")
 
 library(IDPmisc)
 ##need to read the data from exported table, cannot use Deseq matrix or result matrix directly##
-ibrary(ggplot2)
+library(ggplot2)
 library(ggrepel)
-f<-read.csv("0h-T5vsL3-T5 for volca.csv")
+# f<-read.csv("0hrvsL3 for volcano.csv")
+#f<-read.csv("InRDN_L3vsL3 Geneid for volca.csv")
+f<-read.csv("5hS-L3-T5vsL3-T5 genename for volca.csv")
 f<-na.omit(f) 
 
 f$threshold = factor(ifelse(f$padj < 0.05 & abs(f$log2FoldChange) >= 2, 
@@ -20,13 +22,13 @@ f$threshold = factor(ifelse(f$padj < 0.05 & abs(f$log2FoldChange) >= 2,
 
 
 
-ggplot(f,aes(x=log2FoldChange,y=-log10(padj),color=threshold))+
+ggobj <- ggplot(f,aes(x=log2FoldChange,y=-log10(padj),color=threshold))+
   geom_point()+
   scale_color_manual(values=c("#CC0000","#2f5688","#BBBBBB"))+#??????????????????
   geom_text_repel(
     data = f[f$padj<0.05&abs(f$log2FoldChange)>=2,],
     aes(label = Label),
-    size = 4,
+    size = 6,max.overlaps = 10000,
     col="black",
     segment.color = "black", show.legend = FALSE )+#??????????????????????????????
   theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(),
@@ -39,3 +41,6 @@ ggplot(f,aes(x=log2FoldChange,y=-log10(padj),color=threshold))+
   geom_vline(xintercept=c(-2,2),lty=3,col="black",lwd=0.5) +#????????????|FoldChange|>=2
   geom_hline(yintercept =c(0,1.3),lty=3,col="black",lwd=0.5)#????????????padj<0.05 
 
+# ggsave("0hrvsL3 for volcano.tiff", device = tiff, height = 8, width = 8,plot = ggobj)
+#ggsave("InRDN_L3vsL3 Geneid for volca.tiff", device = tiff, height = 8, width = 8,plot = ggobj)
+ggsave("5hS-L3-T5vsL3-T5 genename for volca.tiff", device = tiff, height = 8, width = 8,plot = ggobj)
